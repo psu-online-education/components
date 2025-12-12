672fdf863daca416447e69461381c5c62fa769bd
Luke Leber <lal65@psu.edu>
2025-12-12 13:49:09 -0500

    Apply progressive enhancement based approach

--- packages/program-tile/src/js/scripts.js
+++ packages/program-tile/src/js/scripts.js
@@ -6,16 +6,16 @@
       const expand = element.querySelector('.program-tile__expand');
 
       element.addEventListener('component:activate', e => {
-        content.removeAttribute('inert');
-        content.focus();
         if (e?.detail?.disable_animation || window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
           content.style['transition-duration'] = '0ms';
           content.style['height'] = null;
         }
         else {
           content.style['transition-duration'] = Math.min(Math.max(content.scrollHeight / 2, 200), 800) + 'ms';
-          cms.expand(content);
+//          cms.expand(content);
         }
+        content.removeAttribute('inert');
+        content.focus();
       });
 
       element.addEventListener('component:deactivate', e => {
@@ -23,16 +23,16 @@
           content.removeEventListener('transitionend', afterCollapse);
           expand.focus();
         }
-        content.addEventListener('transitionend', afterCollapse);
-        content.setAttribute('inert', '');
         if (e?.detail?.disable_animation || window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
           content.style['transition-duration'] = '1ms';
           content.style['height'] = '5rem';
         }
         else {
           content.style['transition-duration'] = Math.min(Math.max(content.scrollHeight / 2, 200), 800) + 'ms';
-          cms.collapse(content);
+//          cms.collapse(content);
         }
+        content.addEventListener('transitionend', afterCollapse);
+        content.setAttribute('inert', '');
       });
 
       expand.addEventListener('click', () => {
