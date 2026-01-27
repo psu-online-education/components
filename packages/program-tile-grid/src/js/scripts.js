0ecaaf8204c62ecd7bb9f9aa11610db85965c2ac
Luke Leber <lal65@psu.edu>
2026-01-27 14:43:38 -0500

    Tweak example; remove debug

--- packages/program-tile-grid/src/js/scripts.js
+++ packages/program-tile-grid/src/js/scripts.js
@@ -2,11 +2,13 @@
   cms.attach('program-tile-grid', context => {
     const elements = context.querySelectorAll('.program-tile-grid');
     elements.forEach(element => {
+
+      // When an element inside the grid is activated, expand the item.
       element.addEventListener('component:activated', e => {
-        console.log('expanded');
         e.target?.closest('.program-tile-grid__item')?.setAttribute('data-expanded', '');
       });
 
+      // When an element inside the grid is deactivated, collapse the item.
       element.addEventListener('component:deactivated', e => {
         e.target?.closest('.program-tile-grid__item')?.removeAttribute('data-expanded');
       });
