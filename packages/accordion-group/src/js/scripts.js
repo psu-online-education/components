e96d708009cf78876fd068897033f3375d0c3f71
Kyle Leber <kjl16@psu.edu>
2026-09-14 16:41:37 -0400

    Adding in feature to only allow one accordion to be open at a time within a group.

---
+++ packages/accordion-group/src/js/scripts.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Provides group handling for accordions based on data attributes.
+ */
+((cms) => {
+    cms.attach('accordion-group', context => {
+        const accordionGroups = cms.once('accordion-group', '.accordion-group', context);
+
+        accordionGroups.forEach(group => {
+            group.addEventListener('component:activate', e => {
+                if (group.dataset.singleOpen !== 'true') return;
+
+                const activatedAccordion = e.target.closest('.accordion');
+
+                group.querySelectorAll('.accordion').forEach(child => {
+                    if (child !== activatedAccordion) {
+                        child.dispatchEvent(new CustomEvent('component:deactivate', {
+                            detail: e?.detail,
+                        }));
+                    }
+                });
+            }, true);
+        });
+    });
+})(cms);
