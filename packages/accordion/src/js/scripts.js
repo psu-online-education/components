827848a8144f9f2fb902eda5300f8e7d1214c936
Kyle Leber <kjl16@psu.edu>
2026-09-23 16:36:28 -0400

    ...??

--- packages/accordion/src/js/scripts.js
+++ packages/accordion/src/js/scripts.js
@@ -10,16 +10,38 @@
       const button = accordion.querySelector('.accordion__button');
       const content = accordion.querySelector('.accordion__expandable-content');
 
-      accordion.addEventListener('component:activate', e => {
+      // Trying to keep transition to JS and height to CSS...
+      const setTransitionDuration = () => {
+        const duration = Math.min(
+          Math.max(content.scrollHeight / 2, 200),
+          800
+        );
 
+        // Set the transition duration as a CSS variable so that
+        // it can be used in the CSS transition property.
+        content.style.setProperty(
+          '--accordion-transition-duration',
+          `${duration}ms`
+        );
+
+        return duration;
+      };
+
+      accordion.addEventListener('component:activate', e => {
         if (e?.detail?.disable_animation || window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-          content.style['transition-duration'] = '0ms';
-          content.style['height'] = null;
+          content.style.setProperty(
+            '--accordion-transition-duration',
+            '0ms'
+          );
+          content.style.visibility = 'visible';
+          content.style.height = null;
+
           accordion.classList.add('accordion--expanded');
           button.setAttribute('aria-expanded', 'true');
         }
         else {
-          content.style['transition-duration'] = Math.min(Math.max(content.scrollHeight / 2, 200), 800) + 'ms';
+          setTransitionDuration();
+          content.style.visibility = 'visible';
           accordion.classList.add('accordion--expanded');
           button.setAttribute('aria-expanded', 'true');
           cms.expand(content);
@@ -28,31 +50,46 @@
 
       accordion.addEventListener('component:deactivate', e => {
         if (e?.detail?.disable_animation || window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-          content.style['transition-duration'] = '0ms';
-          content.style['height'] = '0';
+          content.style.setProperty(
+            '--accordion-transition-duration',
+            '0ms'
+          );
+          content.style.height = '0';
+          content.style.visibility = 'hidden';
+
           accordion.classList.remove('accordion--expanded');
           button.setAttribute('aria-expanded', 'false');
         }
         else {
-          content.style['transition-duration'] = Math.min(Math.max(content.scrollHeight / 2, 200), 800) + 'ms';
+          const duration = setTransitionDuration();
+
+          content.style.visibility = 'visible';
           accordion.classList.remove('accordion--expanded');
           button.setAttribute('aria-expanded', 'false');
           cms.collapse(content);
+          window.setTimeout(() => {
+            if (!accordion.classList.contains('accordion--expanded')) {
+              content.style.visibility = 'hidden';
+            }
+          }, duration);
         }
       });
 
       button.addEventListener('click', () => {
-
         const state = button.getAttribute('aria-expanded');
+
         if (state === 'true') {
-          accordion.dispatchEvent(new CustomEvent('component:deactivate'));
+          accordion.dispatchEvent(
+            new CustomEvent('component:deactivate')
+          );
         }
         else {
           accordion.dispatchEvent(new CustomEvent('component:activate', {
-            detail: {
-              activation_type: 'USER_ACTIVATE',
-            }
-          }));
+              detail: {
+                activation_type: 'USER_ACTIVATE',
+              }
+            })
+          );
         }
       });
     });
